6888b8e3360ae7d27b809c46be3e7fc6617ef4b8
Whittney2me <wtwome13@gmail.com>
2017-06-26 21:52:28 -0700

    Name changes, etc
    File name changes and condensing into one folder and small aesthetic changes inside

--- sign-up-page/sign-up-page.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-  $(".menu-toggle").on('click', function() {
-    $(this).toggleClass("on");
-    $('.menu-section').toggleClass("on");
-    $("nav ul").toggleClass('hidden');
-  });
-});
